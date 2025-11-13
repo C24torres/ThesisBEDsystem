1c7aa80473fd09654a6f0ca86d1e88234152d316
C24torres <charlietorres09566701327@gmail.com>
2025-11-13 09:48:28 +0800

    updated

---
+++ includes/navbar.php
@@ -0,0 +1,87 @@
+
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+  </div>
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="index.php" class="nav-link">Home</a>
+      </li>
+
+    </ul>
+
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+            <a class="nav-link" data-toggle="dropdown" href="#">
+                <img style="width: 30px; height: 30px;" src="data:image/jpeg;base64,<?php echo base64_encode($row['img']) ?>" class="user-image img-circle img-size-32">
+                <span class="badge badge-warning navbar-badge"></span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <div class="dropdown-divider"></div>
+                <div class="dropdown-item">
+                    <div class="media">
+                        <?php
+                        if (!empty($row['img'])) {
+                        ?>
+                        <img style="width: 50px; height: 50px;" src="data:image/jpeg;base64,<?php echo base64_encode($row['img']) ?>" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                        <?php
+                        } else {
+                        ?>
+                        <img style="width: 50px; height: 50px;" src="../../docs/assets/img/user.png" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                        <?php
+                        }
+                        ?>
+                    <div class="media-body">
+                        <h3 class="dropdown-item-title">
+                            <b>
+                                <?php echo $_SESSION['name']; ?>
+                            </b>
+                        <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+                        </h3>
+                        <p class="text-sm"><?php echo $row['email']?></p>
+                        <p class="text-sm text-muted"><i class="far fa-user mr-1"></i><?php echo $row['username']?></p>
+                    </div>
+                    </div>
+                </div>
+                <div class="dropdown-divider"></div>
+                <a href="../user/edit.account.php" class="dropdown-item">
+                    <i class="fas fa-user mr-2"></i> Edit Account
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="../login/userData/ctrl.logout.php" class="dropdown-item dropdown-footer"><b>Log Out</b></a>
+            </div>
+        </li>
+    </ul>
+    <!-- Right navbar links
+    <ul class="navbar-nav ml-auto">
+           Navbar Search
+      <li class="nav-item">
+        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+          <i class="fas fa-search"></i>
+        </a>
+        <div class="navbar-search-block">
+          <form class="form-inline">
+            <div class="input-group input-group-sm">
+              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+              <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                  <i class="fas fa-search"></i>
+                </button>
+                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                  <i class="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </li>
+    </ul> -->
+  </nav>
+  <!-- /.navbar -->
